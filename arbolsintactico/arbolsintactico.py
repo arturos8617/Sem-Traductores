4390ac8252f1fee22f513ccbc51ddfcdf14ab2dd
arturos8617 <71295425+arturos8617@users.noreply.github.com>
2024-04-24 22:34:09 -0600

    Update arbolsintactico.py
    arbol sintactico modficacion

--- arbolsintactico/arbolsintactico.py
+++ arbolsintactico/arbolsintactico.py
@@ -2,33 +2,37 @@ import ast
 import graphviz
 
 # Función para visualizar el árbol sintáctico
-def visualize_ast(node, graph=None):
+def visualize_ast(node, graph=None, node_counts=None, parent_name=None):
     if graph is None:
-        graph = graphviz.Digraph('AST', node_attr={'shape': 'rectangle'})
-        
+        graph = graphviz.Digraph('AST', node_attr={'shape': 'ellipse'}, format='png')
+    if node_counts is None:
+        node_counts = {}
+
     if isinstance(node, ast.AST):
-        name = f'{type(node).__name__}-{id(node)}'
-        graph.node(name, label=type(node).__name__)
+        node_type = type(node).__name__
+        # Incrementar el conteo para el tipo de nodo o inicializarlo si aún no existe
+        count = node_counts.get(node_type, 0) + 1
+        node_counts[node_type] = count
+        
+        # Crear una etiqueta única para el nodo
+        name = f'{node_type}{count}'
+        graph.node(name, label=node_type)
+        
+        # Conectar este nodo con su padre si es que existe
+        if parent_name is not None:
+            graph.edge(parent_name, name)
         
         for field, value in ast.iter_fields(node):
-            value_name = f'{field}-{id(value)}'
-            graph.node(value_name, label=str(value))
-            graph.edge(name, value_name)
-            
             if isinstance(value, list):
                 for item in value:
-                    child_name = f'{type(item).__name__}-{id(item)}'
-                    visualize_ast(item, graph)
-                    graph.edge(value_name, child_name)
+                    visualize_ast(item, graph, node_counts, parent_name=name)
             elif isinstance(value, ast.AST):
-                child_name = f'{type(value).__name__}-{id(value)}'
-                visualize_ast(value, graph)
-                graph.edge(value_name, child_name)
+                visualize_ast(value, graph, node_counts, parent_name=name)
 
     return graph
 
 # Ruta al archivo de código fuente
-file_path = './example_code.txt'
+file_path = 'example_code.txt'  # Asegúrate de que la ruta al archivo y su nombre sean correctos.
 
 # Leer el código fuente desde el archivo
 with open(file_path, 'r') as source_file:
@@ -39,4 +43,4 @@ parsed_code = ast.parse(code)
 
 # Visualizar el AST
 graph = visualize_ast(parsed_code)
-graph.render('ast-graph', view=True)  # Esto guardará y abrirá el archivo .pdf del gráfico
+graph.view()  # Esto abrirá el gráfico directamente sin guardar un archivo
